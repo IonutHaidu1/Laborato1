69265c7e2b423dd933ec3714daae33266c24d1d1
IonutHaidu1 <Haidu.Ma.Ionut@student.utcluj.ro>
2024-11-04 00:03:32 +0200

    laborator1

--- tema4.py
+++ tema4.py
@@ -4,11 +4,8 @@ cuvant_de_ghicit = random.choice(cuvinte)
 progres = ["_" for _ in cuvant_de_ghicit]
 incercari_ramase = 6
 litere_incercate = []
-
-#nu afisa ca si lista, afiseaza ca string cu join
-#unstring=""
-#print (unstring.join(progres))
 unstring=""
+numaratoare =0
 
 while incercari_ramase:
     print("Introduceti o litera")
@@ -17,21 +14,31 @@ while incercari_ramase:
     litera=input()
     if not litera.isalpha():
         print("Introduceti o litera!!!")
-    else:
-       continue
+    if litera in litere_incercate:
+        print (f"Ai mai incercat aceasta litera")
+
     litere_incercate.append(litera)
     if litera in cuvant_de_ghicit:
+        numaratoare += 1
         for i in range(len(cuvant_de_ghicit)):
             if litera == cuvant_de_ghicit[i]:
                 pozitielitera = i
-                progres[pozitielitera]=litera
+                progres[pozitielitera] = litera
+                litere_incercate.append(litera)
 
     else:
         print ("Litera este gresita")
-        litere_incercate.extend(litera)
-        print (f"Literele gresite pe care le-ai incercat sunt: {litere_incercate}")
-    incercari_ramase = incercari_ramase - 1
+        litere_incercate.append(litera)
+        print (f"Literele pe care le-ai incercat pana acum sunt: {litere_incercate}")
+        incercari_ramase = incercari_ramase - 1
+        print (f"Mai ai {incercari_ramase} incercari")
+    if numaratoare== len(cuvant_de_ghicit):
+      break
 
+if numaratoare == len(cuvant_de_ghicit):
+        print(f"Felicitari! Ai gasit cuvantul!")
+else:
+        print (f"Ai pierdut! Cuvantul era: {cuvant_de_ghicit}")
 
 
 
